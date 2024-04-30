986aecc0da34535cd186501cebd1e28be2040161
VladimirRepp <57412538+VladimirRepp@users.noreply.github.com>
2024-05-01 01:54:48 +0700

    Add files via upload

---
+++ С логгером/DatabaseManager.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Data.OleDb;
+
+namespace Confectionery
+{
+    enum RequestStatus
+    {
+        Empty = 0,
+        Error,
+        Done
+    }
+
+    class DatabaseManager
+    {
+        // Поля класса
+        private string _connectionString = "provider=Microsoft.Jet.OLEDB.4.0;Data Source=Database.mdb"; // строка соеденения
+        private OleDbConnection _dbConnection; 
+        private OleDbDataReader _dbReader;
+
+        // Свойства класса
+        public OleDbConnection Connection => _dbConnection;
+        public OleDbDataReader DataReader => _dbReader;
+
+        // Методы класса
+        public DatabaseManager()
+        {
+            _dbConnection = new OleDbConnection(_connectionString); // создаем соеденение
+        }
+
+        public void OpenConnection()
+        {
+            if (_dbConnection.State != System.Data.ConnectionState.Open)
+                _dbConnection.Open();
+        }
+
+        public void CloseConnection()
+        {
+            if (_dbConnection.State != System.Data.ConnectionState.Closed)
+                _dbConnection.Close();
+        }
+
+        public void CloseReader()
+        {
+            if (!_dbReader.IsClosed)
+                _dbReader.Close();
+        }
+
+        public RequestStatus ExecuteCommand(string query)
+        {
+            RequestStatus status = RequestStatus.Empty;
+
+            try
+            {
+                OpenConnection();
+                OleDbCommand dbCommand = new OleDbCommand(query, _dbConnection); // команда
+
+                // Выполнение команды
+                if (dbCommand.ExecuteNonQuery() == 1)
+                {
+                    status = RequestStatus.Done;
+                }
+                else
+                {
+                    status = RequestStatus.Error;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                CloseConnection();
+            }
+
+            return status;
+        }
+
+        public OleDbDataReader ExecuteCommandReader(string query)
+        {
+            try
+            {
+                OpenConnection();
+                OleDbCommand dbCommand = new OleDbCommand(query, _dbConnection); // команда
+                _dbReader = dbCommand.ExecuteReader();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            finally
+            {
+                //CloseConnection();
+            }
+
+            return _dbReader;
+        }
+    }
+}
---
+++ С логгером/Logger.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Confectionery
+{
+    // Logger.GetInstance().SetViewPort(toolStripLabel_StatusLoger);
+    // Logger.GetInstance().SetTypeViewPort(EViewPortLogger.ToolStripLabel);
+
+    public class Logger
+    {
+        private ToolStripLabel _toolStripLabel;
+        private EViewPortLogger _typeViewPort;
+
+        private static Logger _instance;
+
+        private Logger()
+        {
+            _toolStripLabel = null;
+            _typeViewPort = EViewPortLogger.MessageBox;
+        }
+
+        public static Logger GetInstance()
+        {
+            if (_instance == null)
+                _instance = new Logger();
+            return _instance;
+        }
+
+        public void SetViewPort(ToolStripLabel label)
+        {
+            _typeViewPort = EViewPortLogger.ToolStripLabel;
+            _toolStripLabel = label;
+        }
+
+        public void SetTypeViewPort(EViewPortLogger typeViewPort)
+        {
+            _typeViewPort = typeViewPort;
+        }
+
+        public void Notify(string message)
+        {
+            switch (_typeViewPort)
+            {
+                case EViewPortLogger.MessageBox:
+                    MessageBox.Show(message, "Внимание!");
+                    break;
+
+                case EViewPortLogger.ToolStripLabel:
+                    if (_toolStripLabel != null)
+                        _toolStripLabel.Text = message;
+                    else
+                        throw new Exception("Строка статуса не установлена в логере для текущего окна!");
+                    break;
+            }
+        }
+    }
+
+    public enum EViewPortLogger
+    {
+        Non,
+        MessageBox,
+        ToolStripLabel,
+        TextBox
+    }
+}
